fb5e4dbe0cf0af958a54d48778d27346ff9d3f4c
Patricio Tubio <77248407+soypato@users.noreply.github.com>
2024-05-30 18:26:36 -0300

    Update Main.java

--- pandas/src/Main.java
+++ pandas/src/Main.java
@@ -1,9 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
-       
+
 }
