6dfa16397495d1ee5db40eac5d38c3792ed1d7a2
iMonety <acunaignacio0418@gmail.com>
2024-05-30 18:26:11 -0300

    Opciones de menu

--- pandas/src/Main.java
+++ pandas/src/Main.java
@@ -1,5 +1,54 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    static Scanner scanner1;
+    public static void main(String[] args)
+    {
+        scanner1= new Scanner(System.in);
+        System.out.println("En que sector desea entrar?");
+        System.out.println("1. Iniciar sesion");
+        System.out.println("2. Registar  nuevo usuario");
+        System.out.println("3. Restablecer contraseña");
+        System.out.println("4. Salir");
+
+        System.out.println("Menu inicio");
+
+        System.out.println("1. Menu de tareas ");
+        System.out.println("2. Menu de recompensas ");
+        System.out.println("3. Menu de misiones");
+        System.out.println("4. Configuraciones");
+        System.out.println("5. Salir");
+
+        System.out.println("Menu Tareas");
+        System.out.println("1. Ver y reanudar tareas");
+        System.out.println("2. Ver historial");
+        System.out.println("3. Crear nueva tarea");
+        System.out.println("5. Modificar tareas");
+        System.out.println("6. Salir");
+
+        System.out.println("Menu Recompensas");
+        System.out.println("1. Ver y reanudar tareas");
+        System.out.println("2. Salir");
+
+        System.out.println("Menu Misiones");
+        System.out.println("1. Ver misiones");
+        System.out.println("2. Salir");
+
+        System.out.println("Menu Misiones");
+        System.out.println("1. Ver misiones");
+        System.out.println("2. Salir");
+
+        System.out.println("Menu Configuracion");
+        System.out.println("1. Cambiar nombre ");
+        System.out.println("2. Cambiar contrasena ");
+        System.out.println("3. Salir");
+
+
+
+
     }
+
+
+
 }
